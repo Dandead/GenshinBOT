d49ee135a1af35c77cd4f3cc92115efaa432f58a
Dandead <61425314+Dandead@users.noreply.github.com>
2021-12-14 18:46:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
-from wishes import *
+from wishes import User
+import asyncio
+import hashlib
 
-init(str(input()))
+
+GLOBAL_LOOP = []
+
+
+async def main():
+	while True:
+		with open("./config/curent_link.txt", "r") as e:
+			lines = e.readlines()
+		with open("./config/curent_link.txt", "w") as r:
+			pass
+		if lines:
+			coro = []
+			for line in lines:
+				md5 = hashlib.sha3_224(line.encode()).hexdigest()
+				print(md5)
+				new = User(md5, line)
+				coro.append(new.start_update_db())
+			await asyncio.gather(*coro)
+		await asyncio.sleep(5)
+		print("new iteration")
+	
+asyncio.run(main())
