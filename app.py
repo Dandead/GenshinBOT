dc17e2f925ecf86f819c53d9395b6fbc26939320
Dandead <61425314+Dandead@users.noreply.github.com>
2022-01-20 20:07:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,125 +1,119 @@
+import os
 import asyncio
 import logging.config
 from src.bot import bot
 
 
 LOGGING_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "format": "%(asctime)s:%(name)s:%(process)d:%(lineno)d " "%(levelname)s %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S"
-        },
-        "detailed": {
-            "format": "%(asctime)s:%(name)s:%(process)d:%(filename)s:%(funcName)s:%(lineno)d " "%(levelname)s %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S"
-        },
-        # "json": {
-        #     "()": "pythonjsonlogger.jsonlogger.JsonFormatter",
-        #     "format": """
-        #             asctime: %(asctime)s
-        #             created: %(created)f
-        #             filename: %(filename)s
-        #             funcName: %(funcName)s
-        #             levelname: %(levelname)s
-        #             levelno: %(levelno)s
-        #             lineno: %(lineno)d
-        #             message: %(message)s
-        #             module: %(module)s
-        #             msec: %(msecs)d
-        #             name: %(name)s
-        #             pathname: %(pathname)s
-        #             process: %(process)d
-        #             processName: %(processName)s
-        #             relativeCreated: %(relativeCreated)d
-        #             thread: %(thread)d
-        #             threadName: %(threadName)s
-        #             exc_info: %(exc_info)s
-        #         """,
-        #     "datefmt": "%Y-%m-%d %H:%M:%S"
-        # },
-    },
-    "handlers": {
-        "databases": {
-            "formatter": "default",
-            "level": "INFO",
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": "log/databases.log",
-            "backupCount": 3
-        },
-        "bot": {
-            "formatter": "default",
-            "level": "INFO",
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": "log/bot.log",
-            "backupCount": 3
-        },
-        "errors": {
-            "formatter": "detailed",
-            "level": "ERROR",
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": "log/errors.log",
-            "backupCount": 2
-        },
-    },
-    "loggers": {
-        "databases": {
-            "level": "INFO",
-            "handlers": [
-                "databases"
-            ]
-        },
-        "handlers": {
-            "level": "INFO",
-            "handlers": [
-                "bot"
-            ]
-        },
-        "parser": {
-            "level": "INFO",
-            "handlers": [
-                "bot"
-            ]
-        },
-        "user": {
-            "level": "INFO",
-            "handlers": [
-                "bot"
-            ]
-        },
-    },
-    "root": {
-        "handlers": [
-            "errors"
-        ]
-    },
+	"version": 1,
+	"disable_existing_loggers": False,
+	"formatters": {
+		"default": {
+			"format": "%(asctime)s:%(name)s:%(process)d:%(lineno)d " "%(levelname)s %(message)s",
+			"datefmt": "%Y-%m-%d %H:%M:%S"
+		},
+		"detailed": {
+			"format": "%(asctime)s:%(name)s:%(process)d:%(filename)s:%(funcName)s:%(lineno)d " "%(levelname)s %(message)s",
+			"datefmt": "%Y-%m-%d %H:%M:%S"
+		},
+		# "json": {
+		#     "()": "pythonjsonlogger.jsonlogger.JsonFormatter",
+		#     "format": """
+		#             asctime: %(asctime)s
+		#             created: %(created)f
+		#             filename: %(filename)s
+		#             funcName: %(funcName)s
+		#             levelname: %(levelname)s
+		#             levelno: %(levelno)s
+		#             lineno: %(lineno)d
+		#             message: %(message)s
+		#             module: %(module)s
+		#             msec: %(msecs)d
+		#             name: %(name)s
+		#             pathname: %(pathname)s
+		#             process: %(process)d
+		#             processName: %(processName)s
+		#             relativeCreated: %(relativeCreated)d
+		#             thread: %(thread)d
+		#             threadName: %(threadName)s
+		#             exc_info: %(exc_info)s
+		#         """,
+		#     "datefmt": "%Y-%m-%d %H:%M:%S"
+		# },
+	},
+	"handlers": {
+		"databases": {
+			"formatter": "default",
+			"level": "INFO",
+			"class": "logging.handlers.RotatingFileHandler",
+			"filename": "log/databases.log",
+			"backupCount": 3
+		},
+		"bot": {
+			"formatter": "default",
+			"level": "INFO",
+			"class": "logging.handlers.RotatingFileHandler",
+			"filename": "log/bot.log",
+			"backupCount": 3
+		},
+		"errors": {
+			"formatter": "detailed",
+			"level": "ERROR",
+			"class": "logging.handlers.RotatingFileHandler",
+			"filename": "log/errors.log",
+			"backupCount": 2
+		},
+	},
+	"loggers": {
+		"databases": {
+			"level": "INFO",
+			"handlers": [
+				"databases"
+			]
+		},
+		"handlers": {
+			"level": "INFO",
+			"handlers": [
+				"bot"
+			]
+		},
+		"parser": {
+			"level": "INFO",
+			"handlers": [
+				"bot"
+			]
+		},
+		"user": {
+			"level": "INFO",
+			"handlers": [
+				"bot"
+			]
+		},
+	},
+	"root": {
+		"handlers": [
+			"errors"
+		]
+	},
 }
 
 
 def logs_files_maker():
-    try:
-        with open("log/bot.log", "x"):
-            pass
-    except:
-        pass
-    try:
-        with open("log/databases.log", "x"):
-            pass
-    except:
-        pass
-    try:
-        with open("log/errors.log.log", "x"):
-            pass
-    except:
-        pass
+	filename = ['log/bot.log', 'log/databases.log', 'log/errors.log']
+	if not os.path.exists(r'log'):
+		os.mkdir(r'log')
+	for file in filename:
+		if not os.path.exists(file):
+			with open(file, 'tx') as d:
+				pass
+
 
 
 if __name__ == '__main__':
-    logs_files_maker()
-    logging.config.dictConfig(LOGGING_CONFIG)
-    try:
-        asyncio.run(bot.main())
-    except (KeyboardInterrupt, SystemExit):
-        pass # logger.error("Bot stopped!")
+	logs_files_maker()
+	logging.config.dictConfig(LOGGING_CONFIG)
+	try:
+		asyncio.run(bot.main())
+	except (KeyboardInterrupt, SystemExit):
+		pass # logger.error("Bot stopped!")
 
